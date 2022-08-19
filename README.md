8d111da32dfbaaa38c318d65a0a5e28aba2e182d
Jan S <salecker@medienmonster.com>
2022-08-19 09:05:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# JavaScript template (including p5.js)
+
+## VS Code Extensions
+ - LivePreview (Microsoft): preview browser inside workspace
+ - JavaScript REPL: used for showing JS console output
+
+### How to setup p5 for VS Code (autocomplete & IntelliSense)
+Put the TypeSctipt definition file into your project and reference them inside your sketch liek this:
+/// <reference path="./TSDef/p5.global-mode.d.ts" />
+"use strict";
+
+<https://breaksome.tech/p5js-editor-how-to-set-up-visual-studio-code/>
---
+++ sketch.js
@@ -0,0 +1,25 @@
+// referencing TypeScript Definitions for autocomplete & IntelliSense
+/// <reference path="./TSDef/p5.global-mode.d.ts" />
+"use strict";
+
+// vanilla JS
+console.log('~ sketch.js ~');
+let poo = 0;
+let slider;
+
+// p5 code starts here
+function setup() {
+  createCanvas(600, 600);
+  print("Hello");
+  slider = createSlider(0, 100, 50, 1);
+}
+
+function draw() {
+  let val = slider.value();
+  background(val);
+  rectMode(CENTER);
+  stroke('rgb(0,255,0)');
+  strokeWeight(5);
+  line(20,20,20,200);
+  noFill();
+}
